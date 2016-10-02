1e49f0fdd43e568acf70dee2faeaaa1a5479ba83
user <user@DESKTOP-6GJR874>
2016-10-02 23:17:21 +0200

    C22

--- src/test/java/MIW/C21Test.java
+++ src/test/java/MIW/C21Test.java
@@ -5,10 +5,17 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class C21Test {
+	
+	C21 c = new C21();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testm1() {
+		assertEquals("m1",c.m1());
+	}
+	
+	@Test
+	public void testm2() {
+		assertEquals("m2",c.m2());
 	}
 
 }
