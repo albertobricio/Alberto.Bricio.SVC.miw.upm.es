6a67ad7705fb429b2ec217790e26a9fdcddb4e3e
user <user@DESKTOP-6GJR874>
2016-10-03 17:15:22 +0200

    C32 y C32Test

---
+++ src/test/java/MIW/C32Test.java
@@ -0,0 +1,16 @@
+package MIW;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class C32Test {
+
+	C32 c = new C32();
+	
+	@Test
+	public void testmA() {
+		assertEquals("mA",c.mA());
+	}
+
+}
