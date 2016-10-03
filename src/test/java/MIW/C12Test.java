3cafebbd1c8e8820095d3dff954e5267f62e9602
user <user@DESKTOP-6GJR874>
2016-10-03 17:09:59 +0200

    C12Test

---
+++ src/test/java/MIW/C12Test.java
@@ -0,0 +1,16 @@
+package MIW;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class C12Test {
+
+	C12 c = new C12();
+	
+	@Test
+	public void testmA() {
+		assertEquals("mA",c.mA());
+	}
+
+}
